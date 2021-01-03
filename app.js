322c33d4357055426577a26db0d31868812d50b6
Irfanmalik26 <56300437+Irfanmalik26@users.noreply.github.com>
2021-01-03 21:27:31 +0500

    [Front-end design and development][Complete]
    Important: Folder of node modules and font awesome NOT added

--- app.js
+++ app.js
@@ -6,22 +6,25 @@ router = express();
 const server = http.createServer(router);
 
 router.use(express.static(path.resolve(__dirname, 'views'))); //We define the views folder as the one where all static content will be served
-router.use(express.urlencoded({extended: true})); //We allow the data sent from the client to be coming in as part of the URL in GET and POST requests
+router.use(express.urlencoded({
+    extended: true
+})); //We allow the data sent from the client to be coming in as part of the URL in GET and POST requests
 router.use(express.json()); //We include support for JSON that is coming from the client
 
 
-router.get("/",(res,req)=>{
+router.get("/", (req, res) => {
     res.render("index");
+});
+
+router.post("/add-product", (req, res) => {
+    console.log(typeof req.body);
 });
 
 
 const host = "127.0.0.1";
 const port = 3000;
 
-server.listen(process.env.PORT || port, process.env.IP || host,function(){
+server.listen(process.env.PORT || port, process.env.IP || host, function () {
     const address = server.address()
-    console.log("Server listening at "+host+":"+port);
+    console.log("Server listening at " + host + ":" + port);
 });
-
-
-
